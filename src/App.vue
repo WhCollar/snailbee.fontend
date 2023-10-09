5473380fc207dd1a29677eac9c93309d72443a23
WhCollar <v.zadvornyj@iqdev.digital>
2023-10-09 09:13:08 +0500

    Base structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import Header from "components/header.vue";
+import Footer from "components/footer.vue";
+</script>
+
 <template>
+  <Header/>
   <RouterView/>
+  <Footer/>
 </template>
 
 <style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Main from 'views/Main.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Main from 'views/main.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'main',
     component: Main
+  },
+  {
+    path: '/briefing',
+    name: 'briefing',
+    component: () => import('views/briefing.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
+});
 
-export default router
+export default router;
